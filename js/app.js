90d4f335780dd9b6d354c9041d8fd82e1b20e1eb
Sudhanva A <sudhanva.asai@gmail.com>
2022-09-02 06:58:35 +0530

    redesign with glass for all section

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = flase;
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
+// shrink navbar
+$(document).on("scroll", function () {
+  if ($(document).scrollTop() > 80) {
+    $(".primary-nav").addClass("shrink");
   } else {
-    menuOpen.classList.remove("open");
-    menuOpen = false;
+    $(".primary-nav").removeClass("shrink");
   }
 });
+
+// Go to top button
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
