af8efd3f2c508329cfce076b54fac52d3553a39f
Sudhanva A <sudhanva.asai@gmail.com>
2022-09-07 00:56:41 +0530

    new logo and its alignments

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-// shrink navbar
-$(document).on("scroll", function () {
-  if ($(document).scrollTop() > 80) {
-    $(".primary-nav").addClass("shrink");
-  } else {
-    $(".primary-nav").removeClass("shrink");
-  }
-});
-
 // Go to top button
 mybutton = document.getElementById("myBtn");
 
